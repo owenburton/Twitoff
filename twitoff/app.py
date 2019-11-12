47bece8c2bf0ae69433899c0c6516a5c91eb5c33
owenburton <owenburton@users.noreply.github.com>
2019-11-12 10:56:09 -0800

    pulled real data using Twitter/Basilica API

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,15 +1,35 @@
 """Main application and routing logic for Twitoff."""
-from flask import Flask
-from .models import DB
+# This file should only have application configuration and 
+# routing logic
+
+# standard order for imports is alphabetical
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = config('ENV')
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff!'
+        users = User.query.all()
+        return render_template(
+            'base.html', 
+            title='Home',
+            users=users)
+    
+    # @app.route('/reset')
+    # def reset():
+    #     DB.drop_all()
+    #     DB.create_all()
+    #     return render_template(
+    #         'base.html', 
+    #         title='DB Reset!',
+    #         users=[])
     
     return app
